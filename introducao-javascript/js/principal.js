bee4a2c04b20ab227e5849112353c4d37af8e16b
Bruna Fidelis <bruhf_nasc@hotmail.com>
2019-04-10 20:18:22 -0300

    Commit2

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,3 +1,22 @@
 console.log("Fui carregado de um arquivo externo.");
 var titulo = document.querySelector(".titulo"); //Função querySelector permite buscar pequenas partes do documento para alterar, não é muito bom procurar por tag ex "h1", mas sim por classes, ID ou por seletor CSS, no caso a .titulo pq o meu "h1" transformei em uma classe. //document (DOM, document JS) se trata do "arquivo" mestre do navegador e dentro dele está tudo referente ao HTML da página toda do site. Ele repesenta todo conteúdo da página. E ele que será  ponte entre o HTML e o JS.
 titulo.textContent = "Aparecida Nutricionista"; //texcontent pega o texto da tag que a gente quer alterar e nela podemos manipular as palavras.
+
+var paciente = document.querySelector("#primeiro-paciente"); //criei uma variavel paciente que vai ter a função de dentro do meu document procurar a ID do primeiro paciente.
+var tdPeso = paciente.querySelector(".info-peso"); // criei a variavel tdPeso pra dentro da minha variavel paciente ele procurar pela td info-peso pra trazer essa informação. 
+
+var peso = tdPeso.textContent; //criei a variavel peso pra que ela buscasse somente o valor em texto da variavel tdPeso
+
+console.log(paciente);//apresenta td (paciente)
+console.log(tdPeso);//tag peso
+console.log(peso);//100
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+console.log(tdAltura);
+console.log(altura);
+
+var imc = peso / altura * altura;
+
+console.log(imc);
