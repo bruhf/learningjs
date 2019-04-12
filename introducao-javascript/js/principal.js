42973824d30cfed76934a24227705d3c84d7b999
Bruna Fidelis <bruhf_nasc@hotmail.com>
2019-04-11 21:04:49 -0300

    alt1

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -4,7 +4,6 @@ titulo.textContent = "Aparecida Nutricionista"; //texcontent pega o texto da tag
 
 var paciente = document.querySelector("#primeiro-paciente"); //criei uma variavel paciente que vai ter a função de dentro do meu document procurar a ID do primeiro paciente.
 var tdPeso = paciente.querySelector(".info-peso"); // criei a variavel tdPeso pra dentro da minha variavel paciente ele procurar pela td info-peso pra trazer essa informação. 
-
 var peso = tdPeso.textContent; //criei a variavel peso pra que ela buscasse somente o valor em texto da variavel tdPeso
 
 console.log(paciente);//apresenta td (paciente)
@@ -13,7 +12,6 @@ console.log(peso);//100
 
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
-
 console.log(tdAltura);
 console.log(altura);
 
@@ -23,16 +21,16 @@ var pesoevalido = true;
 var alturaevalida = true;
 
 if (peso < 0 || peso > 1000){
-    alert("Peso Inválido!);
+    console.log("Peso Inválido!);
     pesoevalido = false;
 }
-if (altura < 0 || altura > 3.00){
-    alert("Altura Inválida!);
-    alturaevalida = false;
-}
-if (pesoevalido && alturaevalida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = ímc;
-}
+    if (altura < 0 || altura > 3.00){
+        console.log("Peso Inválido!);
+        alturaevalida = false;
+    }
+        if (pesoevalido && alturaevalida){
+        var imc = peso / (altura * altura);
+        console.log("Peso Inválido!);
+        }
 
 
