d2a39ed3c9bde38989914e2ecc6106bdb166dd1c
Bruna Fidelis <bruhf_nasc@hotmail.com>
2019-04-12 21:55:35 -0300

    FORFUNCTION

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -2,35 +2,44 @@ console.log("Fui carregado de um arquivo externo.");
 var titulo = document.querySelector(".titulo"); //Função querySelector permite buscar pequenas partes do documento para alterar, não é muito bom procurar por tag ex "h1", mas sim por classes, ID ou por seletor CSS, no caso a .titulo pq o meu "h1" transformei em uma classe. //document (DOM, document JS) se trata do "arquivo" mestre do navegador e dentro dele está tudo referente ao HTML da página toda do site. Ele repesenta todo conteúdo da página. E ele que será  ponte entre o HTML e o JS.
 titulo.textContent = "Aparecida Nutricionista"; //texcontent pega o texto da tag que a gente quer alterar e nela podemos manipular as palavras.
 
-var paciente = document.querySelector("#primeiro-paciente"); //criei uma variavel paciente que vai ter a função de dentro do meu document procurar a ID do primeiro paciente.
+var pacientes = document.querySelectorAll(".paciente"); //criei uma variavel paciente que vai ter a função de dentro do meu document procurar a ID do primeiro paciente. Pra procurar ID usa #, classes é .
 var tdPeso = paciente.querySelector(".info-peso"); // criei a variavel tdPeso pra dentro da minha variavel paciente ele procurar pela td info-peso pra trazer essa informação. 
 var peso = tdPeso.textContent; //criei a variavel peso pra que ela buscasse somente o valor em texto da variavel tdPeso
+
+for (var i = 0; i < pacientes.length; i++){ //length é a propriedade que ja tem a informação de quantos objetos tenho dentro de um array
+     console.log(pacientes[i]);
+
+    var paciente = pacientes[i];
+
+     var tdAltura = paciente.querySelector(".info-altura");
+     var altura = tdAltura.textContent;
+     console.log(tdAltura);
+     console.log(altura);
+     
+     var tdImc = paciente.querySelector(".info-imc");
+     
+     var pesoEValido = true;
+     var alturaEValida = true;
+     
+     if (peso < 0 || peso > 1000){
+         console.log("Peso Inválido!);
+         pesoEValido = false;
+     }
+         if (altura < 0 || altura > 3.00){
+             console.log("Peso Inválido!);
+             alturaEValida = false;
+         }
+             if (alturaEValida && pesoEValido) {
+                 var imc = peso / (altura * altura);
+                 tdImc.textContent = imc.toFixed(2);    
+             } 
+        
+}
 
 console.log(paciente);//apresenta td (paciente)
 console.log(tdPeso);//tag peso
 console.log(peso);//100
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-console.log(tdAltura);
-console.log(altura);
 
-var tdImc = paciente.querySelector(".info-imc");
-
-var pesoevalido = true;
-var alturaevalida = true;
-
-if (peso < 0 || peso > 1000){
-    console.log("Peso Inválido!);
-    pesoevalido = false;
-}
-    if (altura < 0 || altura > 3.00){
-        console.log("Peso Inválido!);
-        alturaevalida = false;
-    }
-        if (pesoevalido && alturaevalida){
-        var imc = peso / (altura * altura);
-        console.log("Peso Inválido!);
-        }
 
 
